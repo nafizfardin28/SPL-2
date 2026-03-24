2e9e5844a9c47578ecdd2129b1b6b848bedba5c4
Nafiz Mahmud Fardin <bsse1528@iit.du.ac.bd>
2026-03-24 12:16:35 +0600

    Modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+//import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
+import ForgotPassword from "./pages/Auth/ForgotPassword";
 import Login from "./pages/Auth/Login";
+import OtpVerification from "./pages/Auth/OtpVerification";
 import Register from "./pages/Auth/Register";
 import Submission from "./pages/Auth/Submission";
 import StudentLayout from "./layouts/StudentLayout";
+import AdminDashboard from "./pages/Admin/AdminDashboard";
+import StaffDashboard from "./pages/Staff/StaffDashboard";
+import StudentConfirmation from "./pages/Student/StudentConfirmation";
 import StudentDashboard from "./pages/Student/StudentDashboard";
+import StudentActivityCertificate from "./pages/Student/StudentECA";
 import StudentNotices from "./pages/Student/StudentNotices";
 import StudentPayment from "./pages/Student/StudentPayment";
 import StudentProfile from "./pages/Student/StudentProfile";
 import StudentTestimonial from "./pages/Student/StudentTestimonial";
-import StudentConfirmation from "./pages/Student/StudentConfirmation";
-import StudentActivityCertificate from "./pages/Student/StudentECA";
+
+import TeacherDashboard from "./pages/Teacher/TeacherDashboard";
+import MainLayout from "./layouts/MainLayout";
+
 function App() {
   return (
     <BrowserRouter>
@@ -19,8 +29,17 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/submission" element={<Submission />} />
+        <Route path="/verify-otp" element={<OtpVerification />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
 
-        <Route path="/student" element={<StudentLayout />}>
+        <Route
+          path="/student"
+          element={
+            <ProtectedRoute allowedRoles={["student"]}>
+              <MainLayout />
+            </ProtectedRoute>
+          }
+        >
           <Route path="profile" element={<StudentProfile />} />
           <Route path="dashboard" element={<StudentDashboard />} />
           <Route path="payments" element={<StudentPayment />} />
@@ -29,6 +48,35 @@ function App() {
           <Route path="confirmation" element={<StudentConfirmation />} />
           <Route path="certificate" element={<StudentActivityCertificate />} />
         </Route>
+
+        <Route path="/teacher" element={<MainLayout />}>
+          <Route path="dashboard" element={<TeacherDashboard />} />
+        </Route>
+
+        <Route
+          path="/teacher/dashboard"
+          element={
+            <ProtectedRoute allowedRoles={["teacher"]}>
+              <TeacherDashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/staff/dashboard"
+          element={
+            <ProtectedRoute allowedRoles={["staff"]}>
+              <StaffDashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/admin/dashboard"
+          element={
+            <ProtectedRoute allowedRoles={["superadmin"]}>
+              <AdminDashboard />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
